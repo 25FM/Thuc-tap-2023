9664f8d3ac8090773bb0e59c8cdb2c051fdff2d2
25FM <ddmanh17602@gmail.com>
2023-06-02 13:28:51 +0700

    fix issue python flask configuration

--- DuyManh/python/flask/app.py
+++ DuyManh/python/flask/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/MANH/Documents/GitHub/Thuc-tap-2023/DuyManh/python/flask/templates/test.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -16,6 +17,9 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 # Remove the db.create_all() call from here
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -44,7 +48,7 @@ def delete(id):
     except:
         return 'There was an issue deleting your task'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@app.route('/update/<int:id>', methods= ['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
     if request.method == 'POST':
@@ -60,5 +64,8 @@ def update(id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+    db.create_all()
+    app.run(debug=True)
+
+if __name__ == '__main__':
     app.run(debug=True)
