234491d0be5004e6fab552c97310e84545f765d2
25FM <ddmanh17602@gmail.com>
2023-06-12 15:45:07 +0700

    update app connect mysql, update database column

--- DuyManh/docker/python-docker/app.py
+++ DuyManh/docker/python-docker/app.py
@@ -4,98 +4,100 @@ from datetime import datetime
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+# config database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@127.0.0.1/mytestdb'
 
-# Create SQLAlchemy instance
-db = SQLAlchemy(app)
-
 # config migrate
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Todo(db.Model):
-    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
+    priority = db.Column(db.Integer, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, content, completed, date_created):
-        self.content = content
-        self.completed = completed
-        self.date_created = date_created
-        
-    def __repr__(self):
-        return '<Task %r>' % self.id
-    
     def to_dict(self):
         return {
             'id': self.id,
+            'title': self.title,
             'content': self.content,
+            'completed': self.completed,
+            'priority': self.priority,
             'date_created': self.date_created
         }
+     
         
-        
-db.init_app(app)
 with app.app_context():
-        db.create_all()
-        
-def bubble_sort(tasks):
-    n = len(tasks)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if tasks[j].content > tasks[j + 1].content:
-                tasks[j], tasks[j + 1] = tasks[j + 1], tasks[j]
-    return tasks
+    db.create_all()
 
-@app.route('/', methods=['POST', 'GET'])
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'sort' in request.form:
-            tasks = Todo.query.order_by(Todo.date_created).all()
-            sorted_tasks = bubble_sort(tasks)
-            return render_template('index.html', tasks=sorted_tasks)        
+        # Handle form submission and task creation
+        title = request.form['title']
+        content = request.form['content']
+        priority = request.form['priority']
+        date_created = datetime.utcnow()
+
+        # Create a new task
+        new_task = Todo(title=title, content=content, priority=priority, date_created=date_created)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect('/')  # Redirect to the main page after task creation
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
         
-        if request.headers.get('Content-Type') == 'application/json':
-            task_content = request.json['content']
-        else:
-            task_content = request.form['content']
-        new_task = Todo(content=task_content)
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        complete = 0
+        priority = request.form['priority']
+        date_created = datetime.utcnow()
+        new_task = Todo(title=title, content=content, complete = complete, priority=priority, date_created = date_created)
         try:
             db.session.add(new_task)
             db.session.commit()
-            if request.headers.get('Content-Type') == 'application/json':
-                return jsonify(new_task.to_dict())
-            else:
-                return redirect('/')
+            return redirect('/')
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        if 'Postman' in request.headers.get('User-Agent'):
-            tasks = [task.to_dict() for task in tasks]
-            return jsonify(tasks)
-        else:
-            return render_template('index.html', tasks=tasks)
-
+        return render_template('add.html')
+    
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-        if request.headers.get('Content-Type') == 'application/json':
-            return jsonify(task_to_delete.to_dict())
-        else:
-            return redirect('/')
+        return redirect('/')
     except:
         return 'There was an issue deleting your task'
 
-@app.route('/update/<int:id>', methods= ['GET', 'POST'])
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
     if request.method == 'POST':
+        task.title = request.form['title']
         task.content = request.form['content']
-
+        task.priority = request.form['priority']
         try:
             db.session.commit()
             return redirect('/')
@@ -104,5 +106,6 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
